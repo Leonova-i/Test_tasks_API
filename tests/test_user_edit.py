f4d76e075e960d77180599113a97af9f27ef840e
Leonova-i <irina.leonova1812@gmail.com>
2023-07-07 18:05:45 +0300

    add test in file test_user_edit.py test_edit_just_create_user_without_at

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,9 +1,13 @@
+import allure
+
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase as BC
 from lib.assertions import Assertions
+import allure
 
 
 class TestUserEdit(BC):
+
     def test_edit_just_create_user(self):
         # Register
         register_date = self.prepare_register_data()
@@ -13,7 +17,6 @@ class TestUserEdit(BC):
         Assertions.assert_status_code(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
 
-        # раскладываем соответсвующее данные для будущих запросов
         email = register_date["email"]
         password = register_date['password']
         user_id = self.get_json_value(response1, 'id')
@@ -47,6 +50,33 @@ class TestUserEdit(BC):
             new_name,
             "Wrong name of the user after edit"
         )
+    @allure.story("Ira tests")
+    def test_edit_just_create_user_without_at(self):
 
+        register_date = self.prepare_register_data()
+        data_for_email = self.prepare_register_data_without_at()
+        email_without_dt_for_check = data_for_email["email"]
+        email = register_date["email"]
+        password = register_date['password']
+        login_data_for_check = {'email': email, 'password': password}
 
+        response_create = MyRequests.post('user/', data=register_date)
 
+        Assertions.assert_status_code(response_create , 200)
+        user_id = self.get_json_value(response_create , 'id')
+
+        # LOGIN
+
+        response_login = MyRequests.post('user/login', data=login_data_for_check)
+        auth_sid = self.get_cookie(response_login, 'auth_sid')
+        token = self.get_header(response_login, "x-csrf-token")
+
+        # EDIT
+
+        response_fail_change = MyRequests.put(f'user/{user_id}',
+                                              cookies={'auth_sid': auth_sid},
+                                              headers={"x-csrf-token": token},
+                                              data={'email': email_without_dt_for_check})
+
+        Assertions.assert_status_code(response_fail_change, 400)
+        assert response_fail_change.text == "Invalid email format"
